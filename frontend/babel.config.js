a72822c82dad1cbb4bd370988b8c89e83e2b24ef
MLowen1 <mikelowenuk@gmail.com>
2025-04-09 14:48:15 +0100

    chore: Configure Babel and add React imports for JSX

---
+++ frontend/babel.config.js
@@ -0,0 +1,7 @@
+// frontend/babel.config.js
+export default {
+    presets: [
+      '@babel/preset-env',  // For modern JavaScript features
+      '@babel/preset-react' // For transforming JSX
+    ]
+  };
