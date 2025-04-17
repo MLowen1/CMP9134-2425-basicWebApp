f18f9605982948c8d9ee54ff1b700a2981eb2418
MLowen1 <mikelowenuk@gmail.com>
2025-04-17 15:43:36 +0100

    fix: backend

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 """
-Top-level configuration module that re-exports backend app and database.
+Top-level configuration module that re-exports backend app, database, and token_blocklist.
 """
-from backend.config import app, db
+from backend.config import app, db, token_blocklist
