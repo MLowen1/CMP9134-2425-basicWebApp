005344d9d164cea2c46ccb7244a084f28a2f29da
Mlowen1 <12294679@students.lincoln.ac.uk>
2025-05-03 18:44:44 +0100

    chore: Add type hinting files for various packages

--- backend/.venv/Scripts/activate.bat
+++ backend/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\mikel\OneDrive - University of Lincoln\Documents\University\MSc\CMP9134 Software Engineering\Assessment 1\Webapp\CMP9134-2425-basicWebApp\backend\.venv
+set VIRTUAL_ENV=C:\Users\Student\Documents\Module Dev Containers\CMP9134-2425-basicWebApp\backend\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
